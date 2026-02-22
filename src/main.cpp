92a1d8549b6b12e5e389dfbbe5a9be8f9622e13a
dousx <dousx_cn@163.com>
2026-02-22 16:08:02 +0800

    leetcode intToRoman

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/solution.h"
+using namespace std;
+int main()
+{
+    Solution solution = Solution();
+    cout << "Hello World!" << endl;
+    cout << solution.intToRoman(1994) << endl;
+    return 0;
+}
