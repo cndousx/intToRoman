77ccd209f1d35979fee0c1fa2f25f8eb5b5bb8e1
dousx <dousx_cn@163.com>
2026-02-23 19:59:58 +0800

    clang-format(llvm)

--- include/solution.h
+++ include/solution.h
@@ -1,11 +1,11 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 #include <string>
-/// @brief 将整数转换为罗马数字 https://leetcode.cn/problems/integer-to-roman/description/
-class Solution
-{
+/// @brief 将整数转换为罗马数字
+/// https://leetcode.cn/problems/integer-to-roman/description/
+class Solution {
 public:
-    std::string intToRoman(int num);
+  std::string intToRoman(int num);
 };
 
 #endif // SOLUTION_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
 #include "../include/solution.h"
+#include <iostream>
+
 using namespace std;
-int main()
-{
-    Solution solution = Solution();
-    cout << "Hello World!" << endl;
-    cout << solution.intToRoman(1994) << endl;
-    return 0;
+int main() {
+  Solution solution = Solution();
+  cout << "Hello World!" << endl;
+  cout << solution.intToRoman(1994) << endl;
+  return 0;
 }
